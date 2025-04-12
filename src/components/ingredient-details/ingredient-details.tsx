55f303b1d6cf794d650e60aeb1f23307542846f7
Mercuuury <kovshiksa@gmail.com>
2025-04-12 21:01:22 +0300

    feat: :sparkles: add the ability to view the ingredient by direct link

--- src/components/ingredient-details/ingredient-details.tsx
+++ src/components/ingredient-details/ingredient-details.tsx
@@ -1,15 +1,23 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import { Preloader } from '../ui/preloader';
 import { IngredientDetailsUI } from '../ui/ingredient-details';
 import { useParams } from 'react-router-dom';
-import { useSelector } from '../../services/store';
+import { useDispatch, useSelector } from '../../services/store';
+import { fetchIngredients } from '../../services/slices/ingredientsSlice';
 
 export const IngredientDetails: FC = () => {
+  const dispatch = useDispatch();
+  const { items } = useSelector((state) => state.ingredients);
+
+  useEffect(() => {
+    if (items.length === 0) {
+      dispatch(fetchIngredients());
+    }
+  }, []);
+
   const { id } = useParams<{ id: string }>();
 
-  const ingredientData = useSelector((state) =>
-    state.ingredients.items.find((item) => item._id === id)
-  );
+  const ingredientData = items.find((item) => item._id === id);
 
   if (!ingredientData) {
     return <Preloader />;
