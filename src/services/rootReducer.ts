06bdf7dd2c1ea8702c79de50f5439082e4bda62e
Mercuuury <kovshiksa@gmail.com>
2025-04-12 18:30:04 +0300

    feat: :sparkles: add & implement `userOrdersSlice`

--- src/services/rootReducer.ts
+++ src/services/rootReducer.ts
@@ -2,11 +2,13 @@ import { combineReducers } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
 import feedsReducer from './slices/feedsSlice';
 import ingredientsReducer from './slices/ingredientsSlice';
+import userOrdersReducer from './slices/userOrdersSlice';
 
 const rootReducer = combineReducers({
-  ingredients: ingredientsReducer,
   auth: authReducer,
-  feeds: feedsReducer
+  feeds: feedsReducer,
+  ingredients: ingredientsReducer,
+  userOrders: userOrdersReducer
 });
 
 export default rootReducer;
